c38fa35071793fb4a6b34e803e213369c847b05b
foreverchx <hrewego8520@yahoo.com.tw>
2019-02-23 16:40:03 +0800

    Initial commit

---
+++ src/main/kotlin/com/lin/Hello.kt
@@ -0,0 +1,12 @@
+package com.lin
+
+fun main(args: Array<String>) {
+   // println("Hello lin")
+    //Human().hello()
+    val h=Human()
+    h.hello()
+}
+class Human{
+fun hello(){
+    println("Hello lin")}
+}
